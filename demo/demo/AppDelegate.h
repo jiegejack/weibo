908791061543659cec4dfcfc0bf6f405873e40ab
Your Name <you@example.com>
2020-09-03 00:06:28 +0800

    12

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Other on 2020/9/3.
+//  Copyright © 2020 Other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
