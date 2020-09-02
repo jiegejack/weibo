1b370d13ab79416644a2997c4850f680bbd68b5c
Your Name <you@example.com>
2020-09-02 22:36:58 +0800

    测试代码2

--- demo/demo/AppDelegate.m
+++ demo/demo/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"测试代码1");
+    NSLog(@"测试代码2");
     return YES;
 }
 
